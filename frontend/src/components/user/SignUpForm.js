acb99b93e1a396b421c61744c439a4c75af896b8
Leah Massey <15648177+leah-massey@users.noreply.github.com>
2023-04-13 22:23:07 +0100

    styling of sign up page

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from '../nav/nav';
+import './SignUpForm.css';
 
 const SignUpForm = ({ navigate }) => {
 
@@ -42,14 +43,48 @@ const SignUpForm = ({ navigate }) => {
     return (
       <>
       <Navbar/>
-      <h2>Sign Up</h2>
-      <p>Password: 8 characters minimum</p>
-      <form onSubmit={handleSubmit}>
-          <input placeholder="username" id="username" type="text" value={username} onChange={handleUsernameChange}></input>
-          <input placeholder="Email" id="email" type='text' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" value={ email } onChange={handleEmailChange} />
-          <input placeholder="Password" id="password" type='password' pattern="[a-zA-Z0-9.-_!?]{8,20}" value={ password } onChange={handlePasswordChange} />
-        <input id='submit' type="submit" value="Create account" />
-      </form>
+      <body>
+      
+        <div class="container">
+        
+          <div class="signup-box">
+          <h2>SIGN UP </h2>
+          
+      
+      
+            <div class="header">
+            <h1>Welcome, Earthling!</h1>
+            </div>
+            <div class="user-icon">
+            <i class="fa-solid fa-earth-americas"></i>
+            </div>
+              <form class="signup" onSubmit={handleSubmit}>
+                <div class="form-group">
+                  <input placeholder="username" id="username" type="text" value={username} onChange={handleUsernameChange}></input>
+                </div>
+                <div class="form-group">
+                  <input placeholder="email" id="email" type='text' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" value={ email } onChange={handleEmailChange} />
+                </div>
+                <div class="form-group">
+                  <input placeholder="password" id="password" type='password' pattern="[a-zA-Z0-9.-_!?]{8,20}" value={ password } onChange={handlePasswordChange} />
+                  <p>minimum 8 characters</p>
+                </div>
+            
+                <div class="form-group">
+                  <input id='submit' type="submit" value="Submit" />
+                </div>
+                <div class="form-group">
+                  <p>Already registered? <a href ="login">Login</a></p>
+                </div>
+              </form>
+            </div>
+            <div class="separator">
+
+            </div>
+            
+          
+        </div>
+      </body>
       </>
     );
 }
