5ff41ab3ca93cda451a6393b47896ae2be614349
kamahack <kamila.felger@gmail.com>
2023-04-14 15:22:51 +0200

    homepage

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,5 +1,23 @@
 import Navbar from "./nav/nav";
 
 export default function Home() {
-  return (<Navbar/>);
+  return (
+    <>
+     <Navbar/>
+     <div class="gallery_parent">
+      <div class='gallery'>
+        <img src="/group_pics/bamba.jpeg" alt="Destin"/>
+        <img src="/group_pics/adnan.jpg" alt="Adnan"/>
+        <img src="/group_pics/kassandra.jpg" alt="Kassandra"/>
+        <img src="/group_pics/francesco.jpg" alt="Francesco"/>     
+        <img src="/group_pics/josh.jpg" alt="Josh"/>   
+        <img src="/group_pics/leah.jpeg" alt="Leah"/>   
+    
+    </div>
+    </div>
+
+    <h1 Style="text-align:center;">Acebook, where what's on your mind matters!</h1>
+
+
+    </>);
 };
